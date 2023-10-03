16512cb8162fd877bbe1c76967c4ffefda12cb74
Windows10-1 <146883289+Windows10-1@users.noreply.github.com>
2023-10-03 12:54:57 -0700

    yes

---
+++ hello-world.py
@@ -0,0 +1,12 @@
+#this is the function system def- print is print
+def PrintHelloWorld():
+	a = "Hello World! I like trains"
+	print(a)
+
+#can i get a public static void Main?
+def main():
+	PrintHelloWorld()
+
+#if i have function named "input", do this
+if __name__=="__main__":
+	main()
